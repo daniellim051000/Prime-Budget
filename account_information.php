ce4c1f282942ff3137515b35e6aa4d4d4769c17d
Daniel Lim Zheng Yu <daniellim051000@gmail.com>
2020-07-20 15:07:03 +0800

    Delete extra space

--- account_information.php
+++ account_information.php
@@ -8,21 +8,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-
   <title>Account Information</title>
-
   <!-- Custom fonts for this template -->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template -->
   <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
   <!-- Custom styles for this page -->
   <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
   <link href="css/style.css" rel="stylesheet">
-
-
 </head>
 
 <body id="page-top">
@@ -30,7 +25,7 @@
 <?php 
 	include("session.php");
  ?>
- 
+
   <!-- Page Wrapper -->
   <div id="wrapper">
 
@@ -77,7 +72,7 @@
           </div>
         </div>
       </li>
-
+      
       <!-- Nav Item - Utilities Collapse Menu -->
       <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
